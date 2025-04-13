d5b85defea7eaa108e2753f1bf9b80a20103a9b5
tanyazinovieva <zinovievatanya123@gmail.com>
2025-04-13 22:03:52 +0300

    запсисбэк и фронт запускаются вместÐе, тестирование не проведенÐоÐ

--- main.py
+++ main.py
@@ -1,47 +1,63 @@
-from fastapi import FastAPI, HTTPException
-from typing import Dict, Optional, List
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, Depends, Path, Query, Body
+from typing import List, Dict, Optional, List, Annotated
+from sqlalchemy.orm import Session
 
-app = FastAPI()  # создаем обект этого класса не передаем параметры
+from fastapi.middleware.cors import CORSMiddleware
 
+from models import Base, User, Post
+from database import engine, session_local
+from schemas import UserCreate, User as DbUser, PostCreate, PostResponse
 
-class Post(BaseModel):
-    id: int
-    title: str
-    body: str
+app = FastAPI()
 
-
-posts = [
-    {'id': 1, 'title': 'News 1', 'body': 'Text 1'},
-    {'id': 2, 'title': 'News 2', 'body': 'Text 2'},
-    {'id': 3, 'title': 'News 3', 'body': 'Text 3'},
+origins = [
+    "http://localhost:5000",
+    "http://127.0.0.1:5000",
 ]
 
-# @app.get("/items")
-# async def items() -> list[Post]:
-#    post_objects = []
-#    for post in posts:
-#        post_objects.append(Post(id=post['id'], title=post['title'], body=post['body']))
-#     return posts
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+Base.metadata.create_all(bind=engine)
 
-@app.get("/items")
-async def items() -> list[Post]:
-    return [Post(**post) for post in posts]
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/items/{id}")
-async def items(id: int) -> Post:
-    for post in posts:
-        if post['id'] == id:
-            return Post(**post)
+# Пример GET-запроса
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
 
-    raise HTTPException(status_code=404, detail='POST not found')
+@app.post("/users/", response_model=DbUser)
+async def create_user(user:  UserCreate, db: Session = Depends(get_db)) -> DbUser:
+    db_user = User(name=user.name, age=user.age)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
 
-@app.get("/search")
-async def search(post_id: Optional[int] = None) -> Dict[str, Optional[Post]]:
-    if post_id:
-        for post in posts:
-            if post['id'] == post_id:
-                return {"data": Post(**post)}
-        raise HTTPException(status_code=404, detail='POST not found')
-    else:
-        return {"data": None}
+    return db_user
+
+@app.post("/posts/", response_model=PostResponse)
+async def create_post(post: PostCreate, db: Session = Depends(get_db)) -> PostResponse:
+    db_user = db.query(User).filter(User.id == post.author_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    db_post = Post(title=post.title, body=post.body, author_id=post.author_id)
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+
+    return db_post
+
+@app.get("/posts/", response_model=List[PostResponse])
+async def posts(db: Session = Depends(get_db)):
+    return db.query(Post).all()
